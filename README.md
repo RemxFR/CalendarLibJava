26c2e0660f96ae8631c819c15fd4088ee338d5b0
RemxFr <epourexisterpro@gmail.com>
2023-10-12 08:51:53 +0200

    feat(Ajout_Commentaires_et_README) - Ajout de commentaire pour simplifier la compréhension de la librairie; - Création d'un fichier README pour facilité son utilisation.
    ref: Création de commentaires et du README

---
+++ README.md
@@ -0,0 +1,19 @@
+# CalendarLibJava
+
+## Fonctionnement
+En récupérent un mois et une année et possiblement un évèneent, cette api renvoi un calendrier mensuel
+et peut associer au jour choisi l'évènement demander.
+L'évènement ici n'est pas un event au sens de programmation, mais ue chose à faire.
+
+## Installation
+Télécharger le dépôt git et inclure la librairie dans le dossier .m2 de votre ordinateur, afin d'avoir accès à cette
+librairie dans votre projet java.
+La librairie fonctionne avec la JDK 1.8.
+Pour l'inclure à votre projet, veuillez ajouter dans les ''dependancies'' de votre fichier POM.xml:
+`<dependency>
+<groupId>org.my_monthly_calendar</groupId>
+<artifactId>CalendarLibJava</artifactId>
+<version>3.0-SNAPSHOT</version>
+</dependency>`
+
+
--- src/main/java/org/example/Calendar.java
+++ src/main/java/org/example/Calendar.java
@@ -11,13 +11,20 @@ public class Calendar {
     private final int MIN_DAY_OF_MONTH = 1;
     private final int MAX_DAY_OF_MONTH = 31;
 
-    public Calendar() {
-    }
+    public Calendar() {}
 
+    /*
+    * Méthode qui retourne une liste de CalendarEntity.
+    * Ici, c'est un calendrier mensuel simple sans event qui est retourné.
+    */
     public List<CalendarEntity> getMonthCalendar(String month, String year) {
         return this.iterateMonth(this.getDate(month, year));
     }
 
+    /*
+     * Méthode qui retourne une liste de CalendarEntity.
+     * Ici c'est un calendrier mensuel avec event qui est retourné.
+     */
     public List<CalendarEntity> generateEventMonthCalendar(Date date, String event) {
         List<CalendarEntity> calendarEntities = new ArrayList<>();
         List<CalendarEntity> monthCalendar = this.iterateMonth(date);
@@ -32,6 +39,9 @@ public class Calendar {
         return calendarEntities;
     }
 
+    /*
+     * Méthode qui retourne la date du premier jour du mois passé en paramètre, de l'année passée en paramètre.
+     */
     private Date getDate(String month, String year) {
         int monthIndex = Integer.parseInt(month) - this.MONTH_INDEX_TO_SUBSTRACT;
         int yearInt = Integer.parseInt(year) - this.YEAR_TO_SUBSTRACT;
@@ -39,6 +49,10 @@ public class Calendar {
         return date;
     }
 
+    /*
+     * Méthode qui génère le calendrier mensuel de la date passé en paramètre.
+     * Retourne une liste de CalendarEntity.
+     */
     private List<CalendarEntity> iterateMonth(Date date) {
         List<CalendarEntity> monthCalendar = new ArrayList<>();
         for (int i = 0; i <= this.MAX_DAY_OF_MONTH; i++) {
